2b725142fdb48321506f32efb2425cbe2f5b4975
Dany AUGUSTIN <augustindany@hotmail.com>
2019-10-15 20:02:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board
 board = [" "," "," "," "," "," "," "," "," "]
 puts display_board( board) # Define display_board that accepts a boardb and prints
-end 
+end
 # out the current state.
